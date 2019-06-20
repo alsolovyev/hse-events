e22ab2eb1232a5d5f28777c23eaa4edb6a474cba
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-20 11:35:59 +0300

    Add events timeline component

---
+++ src/components/EventsTimeline.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>timeline</div>
+</template>
+
+<script>
+export default {
+  name: 'EventsTimeline',
+  props: {
+    events: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="sass"></style>
