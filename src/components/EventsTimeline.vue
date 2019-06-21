5f83b077af1df23d699249e1cab2d7427ba47982
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-21 15:34:12 +0300

    Add events page

--- src/components/EventsTimeline.vue
+++ src/components/EventsTimeline.vue
@@ -1,10 +1,53 @@
 <template>
-  <div>timeline</div>
+  <ul class="timeline">
+    <li class="timeline__item" v-for="(event, i) in events" :key="i"
+      ref="event"
+      @mouseenter="hideItems(i)"
+      @mouseleave="showItems(i)"
+      :class="{ 'timeline__item--in-progress': i % 3 === 1 }">
+      <!-- BEGIN Event title -->
+      <h2 class="timeline__title"><a href="">{{event.title}}</a></h2>
+      <!-- END Event title -->
+
+      <!-- BEGIN Event date -->
+      <p class="timeline__date">{{ event.date }}</p>
+      <!-- END Event date -->
+
+      <ul v-if="event.polls.length !== 0">
+        <!-- BEGIN Poll -->
+        <li v-for="(poll, j) in event.polls" :key="j">
+          <a class="timeline__poll" href="">{{poll.title}}</a>
+        </li>
+        <!-- END Poll -->
+      </ul>
+
+      <!-- BEGIN Empty -->
+      <div title="There are no pollings at this event." class="timeline__empty empty-img" v-else></div>
+      <!-- END Empty -->
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'EventsTimeline',
+  data() {
+    return {
+      test: null
+    }
+  },
+  methods: {
+    hideItems(index) {
+      for(let i = 0, len = this.$refs.event.length; i < len; i++) {
+        if(index !== i) this.$refs.event[i].classList.add('timeline__item--is-hidden')
+      }
+    },
+    showItems() {
+      for(let i = 0, len = this.$refs.event.length; i < len; i++) {
+        this.$refs.event[i].classList.remove('timeline__item--is-hidden')
+      }
+    }
+  },
   props: {
     events: {
       type: Array,
@@ -14,4 +57,115 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.timeline
+  width: 100%
+  max-width: 500px
+  margin: #{$padding * 2} 0
+  // +check
+
+  &__item
+    // +check
+    // margin-bottom: #{$padding * 3}
+    border-left: 1px solid rgba($black, .3)
+    padding: $padding #{$padding*2}
+    transition: opacity .3s
+
+    &:last-child
+      margin-bottom: 0
+
+    &:hover
+      border-color: $blue
+      .timeline__title::before
+        background: $blue
+        border-color: $blue
+      .timeline__poll::before
+        background: $blue
+
+    &--in-progress
+      border-color: $pink
+      .timeline
+        &__date
+          // color: $lime
+        &__title::before
+          background: $pink
+          border-color: $pink
+
+        &__poll::before
+          transform: scale3d(1, 1, 1)
+          background: $pink
+
+    &--is-hidden
+      opacity: .3 !important
+      border-color: rgba($black, .3)
+      .timeline
+        &__date
+          // color: $lime
+        &__title::before
+          background: $white
+          border-color: rgba($black, .3)
+
+  &__title
+    position: relative
+    font-weight: 500
+    font-size: 14px
+
+    &::before
+      content: ''
+      width: 9px
+      height: 9px
+      border-radius: 50%
+      border: 1px solid rgba($black, .3)
+      position: absolute
+      top: .5em
+      left: #{-($padding * 2) - 5}
+      z-index: 10
+      background: $white
+      transition: background .3s, border .3s
+
+  &__date
+    margin: 10px 0 15px 0
+    font-size: 10px
+    line-height: 1
+    transition: color .3s
+
+  &__poll
+    display: block
+    padding: 6px 12px
+    margin-bottom: 10px
+    +br(2)
+    +br-light
+    position: relative
+    font-size: 12px
+    overflow: hidden
+    white-space: nowrap
+    // text-overflow: ellipsis
+    transition: transform .3s
+    &::before,
+    &::after
+      content: ''
+      height: 100%
+      position: absolute
+      top: 0
+    &::before
+      left: 0
+      width: 2px
+      background: $blue
+      transform: scale3d(0, 1, 1)
+      transition: transform .3s, background .3s
+
+    &::after
+      right: 0
+      width: 15%
+      min-width: 20px
+      background-image: linear-gradient(90deg, rgba($white, 0), rgba($white, 1) 75%)
+
+    &:hover
+      transform: translate3d(10px, 0, 0)
+      &::before
+        transform: scale3d(1, 1, 1)
+
+  &__empty
+    width: 150px
+    margin: 0 auto
+</style>
--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -3,12 +3,22 @@
     <!-- BEGIN Title -->
     <app-title title="Today events" :subtitle="date"/>
     <!-- END Title -->
+
+    <!-- BEGIN Timeline -->
+    <events-timeline ref="tl" v-if="events.length > 0" :events="events"/>
+    <!-- END Timeline -->
+
+    <!-- BEGIN Empty -->
+    <div ref="empty" class="events__empty empty-img" title="There are no events today." v-else></div>
+    <!-- END Empty -->
   </main>
 </template>
 
 <script>
+import { TimelineLite } from 'gsap'
 import moment from 'moment'
 import AppTitle from '@/components/AppTitle'
+import EventsTimeline from '@/components/EventsTimeline'
 
 export default {
   name: 'Events',
@@ -16,22 +26,80 @@ export default {
     return {
       date: null,
       interval: null,
+      events: [
+        {
+          title: 'Gas and Oil Production Equipment',
+          date: '02.01.2019',
+          polls: [
+            {
+              id: 1,
+              title: 'Equipment for collection and procession of oil and gas'
+            },
+            {
+              id: 2,
+              title: 'Equipment for water-supplying and sewage utilities and sewage utilities'
+            }
+          ]
+        },
+        {
+          title: 'Oil and Gas Transporting Equipment',
+          date: '02.01.2019',
+          polls: [
+            {
+              id: 1,
+              title: 'Drilling technologies'
+            },
+            {
+              id: 2,
+              title: 'Means and technologies of non-destructive supervision'
+            }
+          ]
+        },
+        {
+          title: 'Service, Activities and Technologies Offered by the Oil and Gas Complex',
+          date: '02.01.2019',
+          polls: []
+        },
+        {
+          title: 'Oil and Gas Transporting Equipment',
+          date: '02.01.2019',
+          polls: [
+            {
+              id: 1,
+              title: 'Drilling technologies'
+            },
+            {
+              id: 2,
+              title: 'Means and technologies of non-destructive supervision'
+            }
+          ]
+        }
+      ]
     }
   },
   methods: {
     /**
      * Get current time
      */
-    getTime() { this.date = moment().format("dddd, MMMM Do, h:mm:ss a") }
+    getDate() { this.date = moment().format("dddd, MMMM Do, h:mm:ss a") }
   },
   mounted() {
+    // Set date
     this.date = moment().format("dddd, MMMM Do, h:mm:ss a")
-    this.interval = setInterval(this.getTime, 1000)
+    // Start a constant update date
+    this.interval = setInterval(this.getDate, 1000)
+
+    const timeline = new TimelineLite({ delay: .8 })
+    if(this.$refs.hasOwnProperty('tl')) {
+      timeline.staggerFromTo(this.$refs.tl.$refs.event, .3, { y: -20, autoAlpha: 0 }, { y: 0, autoAlpha: 1 }, .1)
+    } else {
+      timeline.fromTo(this.$refs.empty, .3, { y: -20, autoAlpha: 0 }, { y: 0, autoAlpha: 1 })
+    }
   },
   beforeDestroy() {
     clearInterval(this.interval)
   },
-  components: { AppTitle }
+  components: { AppTitle, EventsTimeline }
 }
 </script>
 
@@ -39,5 +107,22 @@ export default {
 .events
   display: flex
   flex-direction: column
+  align-items: center
+
+
+  &__empty
+    position: relative
+    margin: auto
+    width: 250px
+    &::after
+      content: 'There are not events today.'
+      width: 100%
+      position: absolute
+      bottom: 0
+      left: 50%
+      transform: translate(-50%, 120%)
+      font-size: 12px
+      text-align: center
+      color: #dddddd
 
 </style>
