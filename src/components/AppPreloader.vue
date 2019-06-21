6d38b7293872829ca56662d20e0062fb0c8bdc3e
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-21 22:48:29 +0300

    Change preloader

--- src/components/AppPreloader.vue
+++ src/components/AppPreloader.vue
@@ -5,8 +5,6 @@
     <span class="preloader__line"></span>
     <span class="preloader__line"></span>
     <span class="preloader__line"></span>
-    <span class="preloader__line"></span>
-    <span class="preloader__line"></span>
   </div>
 </template>
 
@@ -19,48 +17,52 @@ export default {
 <style lang="sass">
 .preloader
   $size: 1px
-  width: 75px
-  height: 75px
+  width: $preloader-size
+  height: $preloader-size
   position: relative
   border-radius: 50%
   border: $size solid $trans
   border-bottom-color: $pink
-  animation: spinCounterclockwise 1s infinite linear
+  animation: spinClockwise 5s infinite linear
+  cursor: progress
 
   &__line
-    $colors: $lime, $blue, $pink, #616CFF, $orange
-    $dirs: top, right, bottom, left
     display: block
     border: $size solid $trans
     border-radius: 50%
     position: absolute
     animation-iteration-count: infinite
     animation-timing-function: linear
-    @for $i from 1 through 7
+    $colors: $lime, $blue, $pink, #616CFF
+    $dirs: top, right, bottom, left
+    $amount: 5
+    @for $i from 1 through $amount
       &:nth-child(#{$i})
         $color: nth($colors, random(length($colors)))
         $dir: nth($dirs, random(length($dirs)))
-        top: #{$size * ($i - 0) * 2}
-        right: #{$size * ($i - 0) * 2}
-        bottom: #{$size * ($i - 0) * 2}
-        left: #{$size * ($i - 0) * 2}
+        $p: 0
+        top: #{$size * ($i - $p) * 2}
+        right: #{$size * ($i - $p) * 2}
+        bottom: #{$size * ($i - $p) * 2}
+        left: #{$size * ($i - $p) * 2}
         @if $dir == 'top'
           border-top-color: $color
-          animation-duration: #{random(3) + 2 + s}
-          animation-name: spinCounterclockwise
+          animation-duration: #{random(10) + s}
+          animation-name: spinClockwise
+          // animation-name: spinCounterclockwise
         @else if $dir == 'right'
           border-right-color: $color
-          animation-duration: #{random(3) + 2 + s}
-          animation-name: spinCounterclockwise
+          animation-duration: #{random(10) + s}
+          animation-name: spinClockwise
+          // animation-name: spinCounterclockwise
         @else if $dir == 'left'
           border-left-color: $color
-          animation-duration: #{random(1) + 2 + s}
+          animation-duration: #{random(10) + s}
           animation-name: spinClockwise
         @else
           border-bottom-color: $color
-          animation-duration: #{random(1) + 2 + s}
+          animation-duration: #{random(10) + s}
           animation-name: spinClockwise
-
 
 
 @keyframes spinClockwise
