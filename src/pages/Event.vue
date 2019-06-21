226b0261048300cfbbb3c15a88f6b138f2fab339
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-21 20:19:27 +0300

    Add base html structure

--- src/pages/Event.vue
+++ src/pages/Event.vue
@@ -1,6 +1,23 @@
 <template>
-  <main class="main event">
-    event: {{event.id}}
+  <main class="main event" data-bg="event" @click="isLoading = !isLoading">
+    <transition name="fade" mode="out-in">
+    <!-- BEGIN Event -->
+    <div class="event__inner" v-if="!isLoading" key="event">
+      <h1 class="event__title">Service, Activities and Technologies Offered by the Oil and Gas Complex</h1>
+      <ul>
+        <li>asd</li>
+        <li>asd</li>
+        <li>asd</li>
+        <li>asd</li>
+      </ul>
+      <p class="event__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam incidunt facere amet exercitationem debitis voluptatum hic vel non. Cumque aut, sunt provident mollitia optio magnam facere vitae reprehenderit sit aliquid.</p>
+    </div>
+    <!-- END Event -->
+
+    <!-- BEGIN Preloader -->
+    <div class="event__preloader" v-else key="preloader">preloader</div>
+    <!-- END Preloader -->
+    </transition>
   </main>
 </template>
 
@@ -9,6 +26,7 @@ export default {
   name: 'Event',
   data() {
     return {
+      isLoading: true,
       event: {
         id: null
       }
@@ -29,5 +47,17 @@ export default {
 </script>
 
 <style lang="sass">
+.event
+
+  &::before
+    content: attr(data-bg)
+    position: fixed
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    font: 900 200px/1 'Poppins', sans-serif
+    text-transform: uppercase
+    color: $grey
+    z-index: -1
 
 </style>
