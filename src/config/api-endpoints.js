596c630a87cba96a19b584792d338e0c6fc03096
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-30 21:41:33 +0300

    Add api endpoint for list of upcoming events

--- src/config/api-endpoints.js
+++ src/config/api-endpoints.js
@@ -1,4 +1,5 @@
 export default {
   'signin': 'https://dev.hes.website/api/v1/login',
-  'signup': 'https://dev.hes.website/api/v1/registration'
+  'signup': 'https://dev.hes.website/api/v1/registration',
+  'events': 'https://dev.hes.website/api/v1/events/'
 }
