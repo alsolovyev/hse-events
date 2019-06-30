bea4591e70d903a7bf34af37d64a22dffd9beb23
Solovyev Aleksey <solovyev.a@icloud.com>
2019-06-30 21:42:47 +0300

    Change props name

--- src/components/event/EventList.vue
+++ src/components/event/EventList.vue
@@ -2,11 +2,11 @@
   <ul v-if="events.length > 0">
     <li ref="items" v-for="(event, index) in events" :key="index" style="margin-bottom: 40px">
       <event-card
-        :eventId  = "event.id       || 1"
-        :title    = "event.title    || 'This event has no title'"
-        :location = "event.location || 'This event has no location'"
-        :date     = "event.date     || '01.01'"
-        :time     = "event.time     || '10:00'"/>
+        :eventId  = "event.id                                 || 1"
+        :title    = "event.title                              || 'This event has no title'"
+        :location = "event.location                           || 'This event has no location'"
+        :date     = "event.date.replace('/', '.').slice(0, 5) || '01.01'"
+        :time     = "event.start_time.slice(0, 5)             || '10:00'"/>
     </li>
   </ul>
   <div class="empty-img" v-else style="width: 150px; margin: 0 auto" title="There are no upcoming events"></div>
