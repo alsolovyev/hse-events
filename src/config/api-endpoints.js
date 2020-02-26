4d3a6cecd400f83dd434d707b908e56e9d2d04bd
Aleksey Solovyev <solovyev.a@icloud.com>
2020-02-26 10:17:40 +0300

    Add dist folder

--- src/config/api-endpoints.js
+++
@@ -1,5 +0,0 @@
-export default {
-  'signin': 'https://dev.hes.website/api/v1/login',
-  'signup': 'https://dev.hes.website/api/v1/registration',
-  'events': 'https://dev.hes.website/api/v1/events/'
-}
